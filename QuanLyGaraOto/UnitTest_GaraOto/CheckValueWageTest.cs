5460a34ea73aaecc6c185d6265b3b36e77f562af
luanle13 <thanhluan130201@gmail.com>
2022-11-03 15:46:49 +0700

    [refactor]: CheckValueWageTest

--- QuanLyGaraOto/UnitTest_GaraOto/CheckValueWageTest.cs
+++ QuanLyGaraOto/UnitTest_GaraOto/CheckValueWageTest.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using NUnit.Framework;
 using QuanLyGaraOto.Model;
 using QuanLyGaraOto.ViewModel;
+using System.Collections.Generic;
 
 namespace UnitTest_GaraOto
 {
@@ -11,6 +12,9 @@ namespace UnitTest_GaraOto
     {
         private _MainFunction _functionTest;
         private ObservableCollection<WAGE> _listWages;
+        private List<string> _listInputValues;
+        private List<string> _listInputNames;
+        private List<bool> _listExpectedOutputs;
 
         [SetUp]
         public void Setup()
@@ -22,47 +26,76 @@ namespace UnitTest_GaraOto
                 new WAGE(){ Wage_Name = "Vật Tư ABC",},
                 new WAGE(){ Wage_Name = "Vật Tư Thành Công",},
             };
+
+            _listInputNames = new List<string>()
+            {
+                "Vật tư Sơn Hà",
+                "",
+            };
+            _listInputValues = new List<string>()
+            {
+                "100000",
+                "a",
+                "",
+                "-100000",
+            };
+
+            _listExpectedOutputs = new List<bool>()
+            {
+                false,
+                true,
+            };
         }
 
         [Test]
         public void checkValueWageUTCID01_Test()
         {
-            string name = "Vật Tư X";
-            string value = "1000";
-            bool add_Result = _functionTest.checkValueWage(name, value, _listWages);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueWage(
+                    _listInputNames[0],
+                    _listInputValues[0], 
+                    _listWages
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueWageUTCID02_Test()
         {
-            string name = "Vật Tư X";
-            string value = "a";
-            bool add_Result = _functionTest.checkValueWage(name, value, _listWages);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueWage(
+                    _listInputNames[0],
+                    _listInputValues[1],
+                    _listWages
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueWageUTCID03_Test()
         {
-            string name = "Vật Tư X";
-            string value = "";
-            bool add_Result = _functionTest.checkValueWage(name, value, _listWages);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueWage(
+                     _listInputNames[0],
+                     _listInputValues[2],
+                     _listWages
+                 );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueWageUTCID04_Test()
         {
-            string name = "";
-            string value = "1000";
-            bool add_Result = _functionTest.checkValueWage(name, value, _listWages);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueWage(
+                    _listInputNames[1],
+                    _listInputValues[0],
+                    _listWages
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueWageUTCID05_Test()
         {
-            string name = "Vật Tư X";
-            string value = "-1000";
-            bool add_Result = _functionTest.checkValueWage(name, value, _listWages);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueWage(
+                    _listInputNames[0],
+                    _listInputValues[3],
+                    _listWages
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
     }
 }
