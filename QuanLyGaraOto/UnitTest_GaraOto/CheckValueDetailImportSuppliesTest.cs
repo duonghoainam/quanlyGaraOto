027136cb15154e5c7b98319a09874d58d254f5b0
luanle13 <thanhluan130201@gmail.com>
2022-11-03 15:08:04 +0700

    [refactor]: CheckValueDetailImportSuppliesTest

--- QuanLyGaraOto/UnitTest_GaraOto/CheckValueDetailImportSuppliesTest.cs
+++ QuanLyGaraOto/UnitTest_GaraOto/CheckValueDetailImportSuppliesTest.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using NUnit.Framework;
 using QuanLyGaraOto.Model;
 using QuanLyGaraOto.ViewModel;
+using System.Collections.Generic;
 
 namespace UnitTest_GaraOto
 {
@@ -10,102 +11,143 @@ namespace UnitTest_GaraOto
     public class CheckValueDetailImportSuppliesTest
     {
         private _MainFunction _functionTest;
+        private List<string> _listInputNames;
+        private List<string> _listInputPrices;
+        private List<string> _listInputAmounts;
+        private List<bool> _listExpectedOutputs;
 
         [SetUp]
         public void Setup()
         {
             _functionTest = new _MainFunction();
+
+            _listInputNames = new List<string>()
+            {
+                "",
+                "Nhớt Alpha Boost",
+            };
+            _listInputPrices = new List<string>()
+            {
+                "",
+                "abc",
+                "-1",
+                "0",
+                "10000",
+            };
+            _listInputAmounts = new List<string>()
+            {
+                "",
+                "abc",
+                "-1",
+                "0",
+            };
+
+            _listExpectedOutputs = new List<bool>()
+            {
+                true,
+                false,
+            };
         }
 
         [Test]
         public void checkValueDetailImportSuppliesUTCID01_Test()
         {
-            string name = "";
-            string price = "250000";
-            string amount = "3";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[0],
+                _listInputPrices[4],
+                _listInputAmounts[3]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID02_Test()
         {
-            string name = "Sơn XY";
-            string price = "";
-            string amount = "3";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[1],
+                _listInputPrices[0],
+                _listInputAmounts[3]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID03_Test()
         {
-            string name = "Sơn XY";
-            string price = "250000";
-            string amount = "";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[1],
+                _listInputPrices[4],
+                _listInputAmounts[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID04_Test()
         {
-            string name = "Sơn XY";
-            string price = "abc";
-            string amount = "3";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[1],
+                _listInputPrices[1],
+                _listInputAmounts[3]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID05_Test()
         {
-            string name = "Sơn XY";
-            string price = "10000";
-            string amount = "abc";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                 _listInputNames[1],
+                 _listInputPrices[4],
+                 _listInputAmounts[1]
+                 );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID06_Test()
         {
-            string name = "Sơn XY";
-            string price = "-1";
-            string amount = "0";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[1],
+                _listInputPrices[2],
+                _listInputAmounts[3]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID07_Test()
         {
-            string name = "Sơn XY";
-            string price = "0";
-            string amount = "0";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[1],
+                _listInputPrices[3],
+                _listInputAmounts[3]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID08_Test()
         {
-            string name = "Sơn XY";
-            string price = "10000";
-            string amount = "abc";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[1],
+                _listInputPrices[4],
+                _listInputAmounts[1]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID09_Test()
         {
-            string name = "Sơn XY";
-            string price = "10000";
-            string amount = "-1";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                _listInputNames[1],
+                _listInputPrices[4],
+                _listInputAmounts[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueDetailImportSuppliesUTCID10_Test()
         {
-            string name = "Sơn XY";
-            string price = "10000";
-            string amount = "0";
-            bool add_Result = _functionTest.checkValueDetailImportSupplies(name, price, amount);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueDetailImportSupplies(
+                 _listInputNames[1],
+                 _listInputPrices[4],
+                 _listInputAmounts[3]
+                 );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
     }
 }
