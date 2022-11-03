618058adffd95598b6d2da9115f5c1ba1fa6049e
luanle13 <thanhluan130201@gmail.com>
2022-11-03 14:10:15 +0700

    [refactor]: CheckValueReceptTest

--- QuanLyGaraOto/UnitTest_GaraOto/CheckValueReceptTest.cs
+++ QuanLyGaraOto/UnitTest_GaraOto/CheckValueReceptTest.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using NUnit.Framework;
 using QuanLyGaraOto.Model;
 using QuanLyGaraOto.ViewModel;
+using System.Collections.Generic;
 
 namespace UnitTest_GaraOto
 {
@@ -10,183 +11,230 @@ namespace UnitTest_GaraOto
     public class CheckValueReceptTest
     {
         private _MainFunction _functionTest;
+        private List<string> _listInputMoneys;
+        private List<string> _listInputReceptDates;
+        private List<string> _listInputEmails;
+        private List<bool> _listExpectedOutputs;
 
         [SetUp]
         public void Setup()
         {
             _functionTest = new _MainFunction();
+
+            _listInputMoneys = new List<string>()
+            {
+                "10000",
+                "9999",
+                "10001",
+                "",
+                "asd",
+                "-20000",
+            };
+            _listInputReceptDates = new List<string>()
+            {
+                DateTime.Now.AddDays(1).ToString(),
+                DateTime.Now.ToString(),
+                DateTime.Now.AddDays(-1).ToString(),
+                "",
+                "dasd",
+            };
+            _listInputEmails = new List<string>()
+            {
+                "datnguyen@gmail.com",
+                "",
+                null,
+            };
         }
 
         [Test]
         public void checkValueReceiptUTCID01_Test()
         {
-            string money = "10000";
-            string receiptDate = "12/12/2022";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[0],
+                _listInputReceptDates[0],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueReceiptUTCID02_Test()
         {
-            string money = "10000";
-            string receiptDate = "12/12/2022";
-            string email = "";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[0],
+                _listInputReceptDates[0],
+                _listInputEmails[1]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueReceiptUTCID03_Test()
         {
-            string money = "10000";
-            string receiptDate = "12/12/2022";
-            string email = null;
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[0],
+                _listInputReceptDates[0],
+                _listInputEmails[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueReceiptUTCID04_Test()
         {
-            string money = "10000";
-            string receiptDate = DateTime.Now.ToString();
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(true));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[0],
+                _listInputReceptDates[1],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[0]));
         }
         [Test]
         public void checkValueReceiptUTCID05_Test()
         {
-            string money = "9999";
-            string receiptDate = "12/12/2022";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[1],
+                _listInputReceptDates[0],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID06_Test()
         {
-            string money = "10001";
-            string receiptDate = "12/12/2022";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[2],
+                _listInputReceptDates[0],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID07_Test()
         {
-            string money = "";
-            string receiptDate = "12/12/2022";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[3],
+                _listInputReceptDates[0],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID08_Test()
         {
-            string money = "asd";
-            string receiptDate = "12/12/2022";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[4],
+                _listInputReceptDates[0],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID09_Test()
         {
-            string money = "-20000";
-            string receiptDate = "12/12/2022";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                 _listInputMoneys[5],
+                 _listInputReceptDates[0],
+                 _listInputEmails[0]
+                 );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID10_Test()
         {
-            string money = "10000";
-            string receiptDate = "11/01/2022";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[0],
+                _listInputReceptDates[2],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID11_Test()
         {
-            string money = "10000";
-            string receiptDate = "";
-            string email = "datnguyen@gmail.com";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[0],
+                _listInputReceptDates[3],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID12_Test()
         {
-            string money = "10000";
-            string receiptDate = "12/12/2022";
-            string email = "dasd";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[0],
+                _listInputReceptDates[4],
+                _listInputEmails[0]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID13_Test()
         {
-            string money = "9999";
-            string receiptDate = "11/01/2022";
-            string email = "";
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[1],
+                _listInputReceptDates[2],
+                _listInputEmails[1]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID14_Test()
         {
-            string money = "9999";
-            string receiptDate = "11/01/2022";
-            string email = null;
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[1],
+                _listInputReceptDates[2],
+                _listInputEmails[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID15_Test()
         {
-            string money = "10001";
-            string receiptDate = "11/01/2022";
-            string email = null;
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[2],
+                _listInputReceptDates[2],
+                _listInputEmails[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID16_Test()
         {
-            string money = "";
-            string receiptDate = "";
-            string email = null;
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[3],
+                _listInputReceptDates[3],
+                _listInputEmails[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID17_Test()
         {
-            string money = "asd";
-            string receiptDate = "";
-            string email = null;
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[4],
+                _listInputReceptDates[4],
+                _listInputEmails[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID18_Test()
         {
-            string money = "-20000";
-            string receiptDate = "dasd";
-            string email = null;
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[5],
+                _listInputReceptDates[4],
+                _listInputEmails[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
         [Test]
         public void checkValueReceiptUTCID19_Test()
         {
-            string money = "asd";
-            string receiptDate = "11/01/2022";
-            string email = null;
-            bool add_Result = _functionTest.checkValueReceipt(money, receiptDate, email);
-            Assert.That(add_Result, Is.EqualTo(false));
+            bool add_Result = _functionTest.checkValueReceipt(
+                _listInputMoneys[4],
+                _listInputReceptDates[2],
+                _listInputEmails[2]
+                );
+            Assert.That(add_Result, Is.EqualTo(_listExpectedOutputs[1]));
         }
     }
 }
